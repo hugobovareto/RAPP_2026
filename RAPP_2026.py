2abccc417fcf6bd0f53a3e987b70e72a338e4ffa
hugobovareto <hugobovareto@gmail.com>
2026-05-08 20:28:09 -0300

    Mudança do código para ser só os dados do relatório de 2025

--- RAPP_2026.py
+++ RAPP_2026.py
@@ -68,114 +68,22 @@ df['CPF_Padronizado'] = (
         )
 )
 
-
-# 3. Concatenar com as 16 planilhas (1 para cada DIREC) dos 'Relatórios de Acompanhamento das Turmas de Progressão Parcial' (SigEduc)
-###########################
-# RELATÓRIO DE ACOMPANHAMENTO DE TURMAS EM PROGRESSÃO PARCIALR (SIGEDUC) - 16 PLANILHAS (1 para cada DIREC) com informações por componente para cada estudante
-# caminho da pasta onde estão os arquivos
-pasta = r"C:\Users\hugob\Downloads\Alunos_RAPP_2026"
-
-# lista todos os arquivos .xlsx da pasta
-arquivos = glob.glob(os.path.join(pasta, "*.xlsx"))
-
-# lista para armazenar os dataframes
-dfs = []
-
-for arquivo in tqdm(arquivos, desc="Processando arquivos"):
-    # lê cada arquivo, pulando as 2 primeiras linhas
-    df_unico = pd.read_excel(arquivo, skiprows=2)
-    dfs.append(df_unico)
-
-# concatena todos em um único dataframe
-df_relatorio_RAPP = pd.concat(dfs, ignore_index=True)
-
-
-# Manter apenas as colunas de interesse:
-df_relatorio_RAPP = df_relatorio_RAPP[
-    ['DIREC', 
-     'ESCOLA',
-     'INEP ESCOLA',
-     'ETAPA ENSINO',
-     'SÉRIE',
-     'COMPONENTE CURRICULAR',
-     'CPF',
-     'ESTUDANTE',
-     'MATRÍCULA']]
-
-
-# Padronizar CPF: manter apenas dígitos, completar com zeros à esquerda e formatar como XXX.XXX.XXX-XX
-df_relatorio_RAPP['CPF_Padronizado'] = (
-    df_relatorio_RAPP['CPF']
-        .astype(str)
-        .str.replace(r'\D', '', regex=True)   # remove tudo que não é dígito
-        .str.zfill(11)                        # completa com zeros à esquerda
-        .str.replace(
-            r'(\d{3})(\d{3})(\d{3})(\d{2})',
-            r'\1.\2.\3-\4',
-            regex=True
-        )
-)
-
-
-# Trocar nomenclatura das séries para padronizar:
-mapeamento = {
-    '1º Período (1ª Série)': '1ª SÉRIE',
-    '2º Período (2ª Série)': '2ª SÉRIE',
-    '3º Período (3ª Série)': '3ª SÉRIE',
-    '6º Ano': '6º ANO',
-    '7º Ano': '7º ANO',
-    '8º Ano': '8º ANO',
-    '9º Ano': '9º ANO',
-    '5º PERÍODO (8° E 9° ANO - ANOS FINAIS)': '9º ANO',
-    '4º PERÍODO (6° E 7° ANO - ANOS FINAIS)': '7º ANO',
-    'TURMA I (6° E 7° ANOS)': '7º ANO',
-    'TURMA II (8° E 9° ANOS)': '9º ANO'
-}
-
-df_relatorio_RAPP['SÉRIE'] = df_relatorio_RAPP['SÉRIE'].replace(mapeamento)
-
-
-# Criar coluna de "ETAPA_RESUMIDA" para indicar Anos Finais ou Ensino Médio, de acordo com a série
-mapeamento_etapa = {
-    '1ª SÉRIE': 'Ensino Médio',
-    '2ª SÉRIE': 'Ensino Médio',
-    '3ª SÉRIE': 'Ensino Médio',
-    '6º ANO': 'Ens. Fund. - Anos Finais',
-    '7º ANO': 'Ens. Fund. - Anos Finais',
-    '8º ANO': 'Ens. Fund. - Anos Finais',
-    '9º ANO': 'Ens. Fund. - Anos Finais'
-}
-
-df_relatorio_RAPP['ETAPA_RESUMIDA'] = df_relatorio_RAPP['SÉRIE'].map(mapeamento_etapa)
-
-
-# Padronizar os nomes das colunas para concatenar os 2 dataframes
-df = df.rename(columns={
-    'ALUNO': 'ESTUDANTE',
-    'CPF ALUNO': 'CPF',
-    'MATRÍCULA ESTUDANTE': 'MATRÍCULA'
-})
-
-
-# Concatenar os 2 dataframes
-df_concat = pd.concat([df, df_relatorio_RAPP], ignore_index=True)
-
-# Excluir coluna 'CPF' para só usar o 'CPF_Padronizado' e evitar confusão
-df_concat = df_concat.drop(columns=['CPF'])
+# Excluir coluna 'CPF ALUNO' para só usar o 'CPF_Padronizado' e evitar confusão
+df = df.drop(columns=['CPF ALUNO'])
 
 # 4. Deixar somente componentes da BNCC e estudantes dos Anos Finais Ensino Fundamental e Ensino Médio (6º ano EF até 3ª Série EM);
 # Nesse caso já estão somente esses valores
 
 # 5. Padronizar CPFs para não ter diferenças de formatação entre as bases;
-# Já está padronizado nos 2 dataframnes e na concatenação na coluna 'CPF_Padronizado'
+# Já está padronizado e só usa 1 base
 
 # 6. Excluir duplicatas da combinação de componente + CPF
 # A base tem alguns erros de mesmo estudante em séries e escolas diferentes
-total_duplicadas = df_concat.duplicated(subset=['CPF_Padronizado', 'COMPONENTE CURRICULAR']).sum()
+total_duplicadas = df.duplicated(subset=['CPF_Padronizado', 'COMPONENTE CURRICULAR']).sum()
 total_duplicadas
 
 
-df_final = df_concat.drop_duplicates(subset=['CPF_Padronizado', 'COMPONENTE CURRICULAR'], keep='first', ignore_index=True)
+df_final = df.drop_duplicates(subset=['CPF_Padronizado', 'COMPONENTE CURRICULAR'], keep='first', ignore_index=True)
 
 
 
@@ -205,33 +113,41 @@ serie
 total_serie = serie['Quantidade de Estudantes Distintos'].sum()
 total_serie
 
+
+# Estudante por Série em cada DIREC
+serie_direc = df_final.groupby(['DIREC', 'SÉRIE'])['CPF_Padronizado'].nunique().reset_index().rename(columns={'CPF_Padronizado': 'Quantidade de Estudantes Distintos'})
+serie_direc
+
+
+# Estudante por componente em cada DIREC
+componente_direc = df_final.groupby(['DIREC', 'COMPONENTE CURRICULAR'])['CPF_Padronizado'].nunique().reset_index().rename(columns={'CPF_Padronizado': 'Quantidade de Estudantes Distintos'})
+componente_direc
+
+
+# Estudante por Componente por Série
+componente_serie = df_final.groupby(['SÉRIE', 'COMPONENTE CURRICULAR'])['CPF_Padronizado'].nunique().reset_index().rename(columns={'CPF_Padronizado': 'Quantidade de Estudantes Distintos'})
+componente_serie
+
+
+# Estudante por Componente, por Série e por DIREC
+componente_serie_direc = df_final.groupby(['DIREC', 'SÉRIE', 'COMPONENTE CURRICULAR'])['CPF_Padronizado'].nunique().reset_index().rename(columns={'CPF_Padronizado': 'Quantidade de Estudantes Distintos'})
+componente_serie_direc
+
+
+
+
 # Juntar os valores por DIREC, Componente e Série e salvar em um arquivo Excel, com cada tabela em uma aba diferente
 with pd.ExcelWriter(r"D:\Scripts_Python\FGV\RAPP_2026\20260508_analises_RAPP.xlsx") as writer:
     df_final.to_excel(writer, sheet_name='Base RAPP', index=False)
-    direc.to_excel(writer, sheet_name='Estudantes por DIREC', index=False)
-    componente.to_excel(writer, sheet_name='Estudantes por Componente', index=False)
-    serie.to_excel(writer, sheet_name='Estudantes por Série', index=False)
+    direc.to_excel(writer, sheet_name='DIREC', index=False)
+    componente.to_excel(writer, sheet_name='Componente', index=False)
+    serie.to_excel(writer, sheet_name='Serie', index=False)
+    serie_direc.to_excel(writer, sheet_name='Serie e DIREC', index=False)
+    componente_direc.to_excel(writer, sheet_name='Componente e DIREC', index=False)
+    componente_serie.to_excel(writer, sheet_name='Componente e Serie', index=False)
+    componente_serie_direc.to_excel(writer, sheet_name='Componente, Serie e DIREC', index=False)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-
-7. Conseguir informações de necessidades especiais por CPF em algum relatório;
-8. Conseguir informações de turno por CPF em algum relatório;
-9. Incluir informações de necessidades especiais e turno na base principal;
-10. Fazer as segmentações e contagem de interesse: estudantes por DIREC; por componente; por turno; por Série; necessidades especiais etc.
-'''
 
